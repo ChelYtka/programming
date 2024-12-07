7292f8d2ead46af893ef431abec18d0f042bccab
Egor Danilkin <edanilkin666@mail.ru>
2024-12-07 22:59:01 +0700

    win5

--- main.cpp
+++ main.cpp
@@ -1,80 +1,28 @@
+#include "World.h"
+#include "black_box/Application.h"
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <tuple>
-#include "ip_filter.h"
 
-int main () {
+int main(int argc, char* argv[]) {
 
-    std::vector<std::tuple<int, int, int, int>> ip_adress;
-
-    // считывает файл
-    ip_adress = readfile("../ip_filter.tsv");
-
-    sort_ip(ip_adress);
-    // задание 1
-    std::cout << "task 1" << std::endl;
-    // выводит все ip
-    for (const auto& ip : ip_adress)
-    {
-        std::cout << std::get<0>(ip) << ".";
-        std::cout << std::get<1>(ip) << ".";
-        std::cout << std::get<2>(ip) << ".";
-        std::cout << std::get<3>(ip) << ".";
-        std::cout << std::endl;
+    if (argc < 2) {
+        std::cerr << "Please, call with one argument "
+                  << "( example: " << argv[0] << " <path to file smile.txt> )";
+        return 1;
     }
-
-    std::cout << std::endl;
-    // задание 2
-    std::cout << "task 2" << std::endl;
-    // проверяет, если ip подходит - выводит
-    for (const auto& ip : ip_adress) {
-        if (std::get<0>(ip) == 1)
-        {
-            std::cout << std::get<0>(ip) << ".";
-            std::cout << std::get<1>(ip) << ".";
-            std::cout << std::get<2>(ip) << ".";
-            std::cout << std::get<3>(ip) << ".";
-            std::cout << std::endl;
-        }
+    try {
+        World w(argv[1]);
+        /**
+         * Код ниже запускает выполнение приложения. Для
+         * выполнения задания нет необходимости изучать
+         * или менять содержимое Application. Для начала
+         * лучше сфокусироваться на Ball.hpp, Ball.cpp и
+         * World.cpp
+         */
+        Application app("physics");
+        app.run(w);
+    } catch (std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return 2;
     }
-
-    std::cout << std::endl;
-    // задание 3
-    std::cout << "task 3" << std::endl;
-    // проверяет, если ip подходит - выводит
-    for (decltype(ip_adress[0]) ip : ip_adress) {
-        if (std::get<0>(ip) == 46)
-        {
-            if (std::get<1>(ip) == 70)
-            {
-                std::cout << std::get<0>(ip) << ".";
-                std::cout << std::get<1>(ip) << ".";
-                std::cout << std::get<2>(ip) << ".";
-                std::cout << std::get<3>(ip) << ".";
-                std::cout << std::endl;
-            }
-        }
-    }
-
-    std::cout << std::endl;
-    // задание 4
-    std::cout << "task 4" << std::endl;
-    // проверяет, если ip подходит - выводит
-    for (const auto& ip : ip_adress) {
-        if (std::get<0>(ip) == 46 ||
-            std::get<1>(ip) == 46 ||
-            std::get<2>(ip) == 46 ||
-            std::get<3  >(ip) == 46 )
-        {
-            std::cout << std::get<0>(ip) << ".";
-            std::cout << std::get<1>(ip) << ".";
-            std::cout << std::get<2>(ip) << ".";
-            std::cout << std::get<3>(ip) << ".";
-            std::cout << std::endl;
-        }
-    }
+    return 0;
 }
