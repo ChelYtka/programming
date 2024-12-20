acc27d3b4af82eb734a9b7df3b2f3bf12904f87e
Egor Danilkin <edanilkin666@mail.ru>
2024-12-20 18:36:21 +0700

    win1

--- main.cpp
+++ main.cpp
@@ -1,28 +1,101 @@
-#include "World.h"
-#include "black_box/Application.h"
-#include <iostream>
+#include "GraphicPrimitive.h"
+#include "DocController.h"
 
-int main(int argc, char* argv[]) {
+// Представление GUI
+void printMenu()
+{
+    std::cout << "\nВыберитe действие:\n";
+    std::cout << "1. Создать документ\n";
+    std::cout << "2. Открыть документ по пути\n";
+    std::cout << "3. Импорт документа\n";
+    std::cout << "4. Экспорт документа\n";
+    std::cout << "5. Добавить примитив\n";
+    std::cout << "6. Удалить примитив\n";
+    std::cout << "7. Показать информацию о текущем документе\n";
+    std::cout << "8. Выход\n";
+    std::cout << "Введите номер действия: ";
+}
 
-    if (argc < 2) {
-        std::cerr << "Please, call with one argument "
-                  << "( example: " << argv[0] << " <path to file smile.txt> )";
-        return 1;
+int main()
+{
+    Controller controller;
+
+    while (true)
+    {
+        printMenu();
+        int choice;
+
+        while (!(std::cin >> choice))
+        {
+            std::cin.clear();                                                   // Сбрасываем состояние потока
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Игнорируем некорректный ввод
+            std::cout << "Неккоректный ввод. \n";
+            printMenu();
+        }
+
+        switch (choice)
+        {
+        case 1:
+            controller.createDocument();
+            break;
+        case 2:
+        {
+            std::cout << "Введите путь к документу: ";
+            std::string path;
+            std::cin >> path;
+            controller.openDocumentByPath(path);
+            break;
+        }
+        case 3:
+        {
+            std::cout << "Введите путь до импортируемого документа: ";
+            std::string path;
+            std::cin >> path;
+            std::cout << "Импорт документа из файла: " << std::endl;
+            controller.importDocument(path);
+            std::cout << std::endl;
+            break;
+        }
+        case 4:
+        {
+            std::cout << "Введите путь до экспортируемого документа: " << std::endl;
+            std::string path;
+            std::cin >> path;
+            controller.exportDocument(path);
+            std::cout << std::endl;
+            break;
+        }
+        case 5:
+        {
+            std::cout << "Введите тип примитива(Circle, Square, Rectangle): " << std::endl;
+            std::string type;
+            std::cin >> type;
+            controller.addGraphicPrimitive(type);
+            break;
+        }
+        case 6:
+        {
+            std::cout << "Введите ID графического примитива для удаления: " << std::endl;
+            int id;
+            std::cin >> id;
+            controller.deleteGraphicPrimitive(id);
+            std::cout << std::endl;
+            break;
+        }
+        case 7:
+        {
+            std::cout << "Показать информацию о текущем документе: " << std::endl;
+            controller.showCurrentDocumentInfo();
+            std::cout << std::endl;
+            break;
+        }
+        case 8:
+        {
+            std::cout << "Выход.\n";
+            return 0;
+        }
+        default:
+            std::cout << "Неккоректный выбор";
+        }
     }
-    try {
-        World w(argv[1]);
-        /**
-         * Код ниже запускает выполнение приложения. Для
-         * выполнения задания нет необходимости изучать
-         * или менять содержимое Application. Для начала
-         * лучше сфокусироваться на Ball.hpp, Ball.cpp и
-         * World.cpp
-         */
-        Application app("physics");
-        app.run(w);
-    } catch (std::exception& ex) {
-        std::cerr << ex.what() << std::endl;
-        return 2;
-    }
-    return 0;
 }
